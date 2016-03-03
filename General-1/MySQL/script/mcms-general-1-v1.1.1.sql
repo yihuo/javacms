b5596e51860a0b727a690b90b1ef856ef690aad7
zhukainet <zhukai@zhukai.net>
2016-03-03 10:09:11 +0800

    fix bug

--- General-1/MySQL/script/mcms-general-1-v1.1.1.sql
+++ General-1/MySQL/script/mcms-general-1-v1.1.1.sql
@@ -1140,6 +1140,7 @@ CREATE ALGORITHM=UNDEFINED  SQL SECURITY DEFINER VIEW `v_people_user` AS select 
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `p_getAllChildren`;
 
+DELIMITER //
 CREATE  PROCEDURE `p_getAllChildren`(`pId` int,`appId`int,`modelId` int)
 BEGIN
 	
@@ -1158,5 +1159,5 @@ BEGIN
   end while ;    
   INSERT tmp_category SELECT c.category_id,c.category_title,c.category_categoryid,0 FROM category  c WHERE c.category_id=pid and c.category_appid=appId and c.category_modelId=modelId;   
   SELECT * FROM tmp_category;
-END;;
-;
+END//
+DELIMITER ;
